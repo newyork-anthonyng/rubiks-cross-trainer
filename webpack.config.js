854ddc9a730c7ab5fcb48b16d0dd72fe200c8e7b
Anthony Ng <newyork.anthonyng@gmail.com>
2017-11-25 18:22:00 -0800

    Set libraryTarget as umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "./dist/index.js"
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd"
   },
   module: {
     loaders: [
